a7f103f20547e4facfb12623568fe9ada58e8284
Stephen Josef Wissow <sjw1000@wildcats.unh.edu>
2021-06-30 13:20:54 -0400

    Resolve Python bulitins import error on Ubuntu 18.04

--- scripts/install_prereq_packages.bash
+++ scripts/install_prereq_packages.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 
-apt install ros-melodic-geographic-msgs ros-melodic-geodesy ros-melodic-pid ros-melodic-joy ros-melodic-mavros ros-melodic-rosmon python-gdal qtpositioning5-dev libqt5svg5-dev libgeos++-dev ros-melodic-image-transport ros-melodic-tf2-ros ros-melodic-tf2-geometry-msgs
+apt install ros-melodic-geographic-msgs ros-melodic-geodesy ros-melodic-pid ros-melodic-joy ros-melodic-mavros ros-melodic-rosmon python-gdal qtpositioning5-dev libqt5svg5-dev libgeos++-dev ros-melodic-image-transport ros-melodic-tf2-ros ros-melodic-tf2-geometry-msgs python-future
+
+# python-future : resolves "ImportError: No module builtins" when running sim_local.launch (applies to melodic branch on Ubuntu 18.04)
